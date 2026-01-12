fe2af1cca5071f74863e1297911866e73d27ec32
Nexus Intern <intern@nexus.dev>
2026-01-12 19:05:44 +0500

    Fix: Export correct DocumentPage component name

--- src/pages/documents/DocumentPage.tsx
+++ src/pages/documents/DocumentPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DocumentChamber } from '../../components/documents/DocumentChamber';
 
-export const DocumentsPage: React.FC = () => {
+export const DocumentPage: React.FC = () => {
   return (
     <div className="max-w-7xl mx-auto p-4 space-y-6">
       <div>
@@ -14,4 +14,4 @@ export const DocumentsPage: React.FC = () => {
   );
 };
 
-export default DocumentsPage;
+export default DocumentPage;
